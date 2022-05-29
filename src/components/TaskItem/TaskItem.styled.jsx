2da7368f467f98f459c408fd4bd0d8d16069a463
roman_orlov <roman.developer@mail.ru>
2022-05-29 15:02:00 +0500

    Update TaskItem.styled.jsx

--- src/components/TaskItem/TaskItem.styled.jsx
+++ src/components/TaskItem/TaskItem.styled.jsx
@@ -1,13 +1,29 @@
 import styledComponents from 'styled-components';
 
 export const TaskItemContainer = styledComponents.div`
-    font-size: 20px;
-    font-weight: 600;
-    color: #ffffff;
+    display: flex;
+    align-items: center;
+
+    column-gap: 20px;
 
     background-color: #1D2125;
     border-radius: 15px;
 
     padding: 30px 40px;
     margin-left: ${props => props.currentNestingLevel * 60}px;
+`;
+
+export const TaskItemLabel = styledComponents.label`
+    display: flex;
+    align-items: center;
+
+    column-gap: 20px;
+
+    margin-right: auto;
+`;
+
+export const TaskItemText = styledComponents.span`
+    font-size: 20px;
+    font-weight: 600;
+    color: #ffffff;
 `;
