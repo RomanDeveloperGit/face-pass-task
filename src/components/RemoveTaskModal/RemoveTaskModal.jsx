e891e82481551c0b8f17775443614017dca84f48
roman_orlov <roman.developer@mail.ru>
2022-05-30 00:31:35 +0500

    Added RemoveTaskModal component

---
+++ src/components/RemoveTaskModal/RemoveTaskModal.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from 'react-redux';
+import taskManagerActionCreators from '../../store/actionCreators/taskManager';
+import Modal from '../UI/Modal/Modal';
+import ModalButton from '../UI/ModalButton/ModalButton';
+import { RemoveTaskDescriptionBox, RemoveTaskDescriptionItem } from './RemoveTaskModal.styled';
+
+const RemoveTaskModal = ({ task, setActive }) => {
+	const dispatch = useDispatch();
+	const confirmRemoveTask = () => dispatch(taskManagerActionCreators.remove(task.id));
+	
+	return (
+		<Modal title="Удаление задачи" setActive={setActive}>
+			<RemoveTaskDescriptionBox>
+				<RemoveTaskDescriptionItem>Задача: {task.text}</RemoveTaskDescriptionItem>
+				<RemoveTaskDescriptionItem>Создана: {new Date(task.createdAt).toLocaleString('ca')}</RemoveTaskDescriptionItem>
+				<RemoveTaskDescriptionItem>Вы уверены, что хотите удалить данную задачу? Дальнейшее восстановление невозможно.</RemoveTaskDescriptionItem>
+			</RemoveTaskDescriptionBox>
+			<ModalButton onClick={confirmRemoveTask}>Удалить</ModalButton>
+		</Modal>
+	);
+};
+
+export default RemoveTaskModal;
---
+++ src/components/RemoveTaskModal/RemoveTaskModal.styled.jsx
@@ -0,0 +1,21 @@
+import styledComponents from 'styled-components';
+
+export const RemoveTaskDescriptionBox = styledComponents.p`
+	display: flex;
+	flex-direction: column;
+
+	margin-bottom: 60px;
+`;
+
+export const RemoveTaskDescriptionItem = styledComponents.span`
+	font-size: 18px;
+	color: #ffffff;
+
+	@media (max-width: 460px) {
+		font-size: 16px;
+	}
+
+	@media (max-width: 360px) {
+		font-size: 14px;
+	}
+`;
