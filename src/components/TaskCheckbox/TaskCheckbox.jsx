ba1a006b0d5f3a2f646e6fa9a2aafea1f479ca77
roman_orlov <roman.developer@mail.ru>
2022-05-29 15:23:18 +0500

    Updated TaskCheckbox component

--- src/components/TaskCheckbox/TaskCheckbox.jsx
+++ src/components/TaskCheckbox/TaskCheckbox.jsx
@@ -4,17 +4,17 @@ import taskManagerActionCreators from "../../store/actionCreators/taskManager";
 import Checkbox from "../UI/Checkbox/Checkbox";
 
 const TaskCheckbox = ({ id, checked }) => {
-    const [isChecked, setChecked] = useState(checked);
-    const dispatch = useDispatch();
+	const [isChecked, setChecked] = useState(checked);
+	const dispatch = useDispatch();
 
 	const setCompleteStatusTask = () => {
-        setChecked(!isChecked);
-        dispatch(taskManagerActionCreators.setCompleteStatus(id));
-    }
-    
-    return (
-        <Checkbox checked={isChecked} onChange={setCompleteStatusTask} />
-    );
+		setChecked(!isChecked);
+		dispatch(taskManagerActionCreators.setCompleteStatus(id));
+	}
+	
+	return (
+		<Checkbox checked={isChecked} onChange={setCompleteStatusTask} />
+	);
 };
 
 export default TaskCheckbox;
--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -4,18 +4,18 @@ import TaskRemoveButton from '../TaskRemoveButton/TaskRemoveButton';
 import { TaskItemContainer, TaskItemLabel, TaskItemText } from './TaskItem.styled';
 
 const TaskItem = ({ task, currentNestingLevel }) => {
-    return (
-        <>
+	return (
+		<>
 			<TaskItemContainer currentNestingLevel={currentNestingLevel}>
-                <TaskItemLabel>
-                    <TaskCheckbox id={task.id} checked={task.isCompleted} />
-                    <TaskItemText>{task.text}</TaskItemText>
-                </TaskItemLabel>
-                <TaskRemoveButton id={task.id} disabled={!task.isCompleted} />
-            </TaskItemContainer>
+				<TaskItemLabel>
+					<TaskCheckbox id={task.id} checked={task.isCompleted} />
+					<TaskItemText>{task.text}</TaskItemText>
+				</TaskItemLabel>
+				<TaskRemoveButton id={task.id} disabled={!task.isCompleted} />
+			</TaskItemContainer>
 			{task.children && <TaskList tasks={task.children} currentNestingLevel={++currentNestingLevel} />}
 		</>
-    );
+	);
 };
 
 export default TaskItem;
