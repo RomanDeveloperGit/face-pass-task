81222cef3df90b46c972c115f07abf0c1054453f
roman_orlov <roman.developer@mail.ru>
2022-05-29 15:01:47 +0500

    Added functional for remove button

--- src/components/TaskRemoveButton/TaskRemoveButton.jsx
+++ src/components/TaskRemoveButton/TaskRemoveButton.jsx
@@ -1,12 +1,17 @@
+import { useDispatch } from 'react-redux';
 import basketSVG from '../../assets/images/icons/basket.svg';
+import taskManagerActionCreators from '../../store/actionCreators/taskManager';
 import { RemoveButton } from './TaskRemoveButton.styled';
 
-const TaskRemoveButton = ({ disabled }) => {
-    return (
-        <RemoveButton disabled={disabled}>
-            <img src={basketSVG} alt="Basket icon" />
-        </RemoveButton>
-    );
+const TaskRemoveButton = ({ id, disabled }) => {
+	const dispatch = useDispatch();
+	const removeTask = () => dispatch(taskManagerActionCreators.remove(id));
+
+	return (
+		<RemoveButton disabled={disabled} onClick={removeTask}>
+			<img src={basketSVG} alt="Basket icon" />
+		</RemoveButton>
+	);
 };
 
 export default TaskRemoveButton;
--- src/components/TaskRemoveButton/TaskRemoveButton.styled.jsx
+++ src/components/TaskRemoveButton/TaskRemoveButton.styled.jsx
@@ -1,17 +1,17 @@
 import styledComponents from 'styled-components';
 
 export const RemoveButton = styledComponents.button`
-    background-color: #CB4041;
-    border-radius: 10px;
+	background-color: #CB4041;
+	border-radius: 10px;
 
-    opacity: ${props => props.disabled ? 0.7 : 1};
-    transition: .5s;
+	opacity: ${props => props.disabled ? 0.7 : 1};
+	transition: .5s;
 
-    padding: 8px;
+	padding: 8px;
 
-    ${props => !props.disabled && `
-        &:hover {
-            background-color: #C22223;
-        }
-    `}
+	${props => !props.disabled && `
+		&:hover {
+			background-color: #C22223;
+		}
+	`}
 `;
