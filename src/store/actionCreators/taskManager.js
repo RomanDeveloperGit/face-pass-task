c7a46535c3c8737881167d54713796d466ffd5cf
roman_orlov <roman.developer@mail.ru>
2022-05-29 14:56:38 +0500

    Update taskManager.js

--- src/store/actionCreators/taskManager.js
+++ src/store/actionCreators/taskManager.js
@@ -1,23 +1,24 @@
+import { generateId } from '../../utils/utils';
 import { ADD_TASK, SET_COMPLETE_STATUS_TASK, REMOVE_TASK } from '../actions/taskManager';
 
 const taskManagerActionCreators = {
-	add: (parentId, text) => ({
+	add: (parentText, text) => ({
 		type: ADD_TASK,
 		payload: {
-			
+			parentText,
+			id: generateId(),
+			text,
+			isCompleted: false,
+			createdAt: new Date().getTime()
 		}
 	}),
 	setCompleteStatus: id => ({
 		type: SET_COMPLETE_STATUS_TASK,
-		payload: {
-			
-		}
+		payload: { id }
 	}),
 	remove: id => ({
 		type: REMOVE_TASK,
-		payload: {
-			
-		}
+		payload: { id }
 	}),
 };
 
