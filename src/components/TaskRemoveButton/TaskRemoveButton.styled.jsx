d6ba98fbdd3e4d648165c9bb22327bbff5d3986f
roman_orlov <roman.developer@mail.ru>
2022-05-29 15:39:35 +0500

    Fixed TaskRemoveButton component

--- src/components/TaskRemoveButton/TaskRemoveButton.styled.jsx
+++ src/components/TaskRemoveButton/TaskRemoveButton.styled.jsx
@@ -1,13 +1,18 @@
 import styledComponents from 'styled-components';
 
 export const RemoveButton = styledComponents.button`
+	width: 36px;
+	height: 36px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
 	background-color: #CB4041;
 	border-radius: 10px;
 
 	opacity: ${props => props.disabled ? 0.7 : 1};
 	transition: .5s;
-
-	padding: 8px;
 
 	${props => !props.disabled && `
 		&:hover {
