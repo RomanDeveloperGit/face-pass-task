a5fb5f69cd8fd1552a85fe7b84a96696b3401d20
roman_orlov <roman.developer@mail.ru>
2022-05-29 17:11:28 +0500

    Update TaskItem.styled.jsx

--- src/components/TaskItem/TaskItem.styled.jsx
+++ src/components/TaskItem/TaskItem.styled.jsx
@@ -10,6 +10,6 @@ export const TaskItemContainer = styledComponents.div`
     background-color: #1D2125;
     border-radius: 15px;
 
-    padding: 30px 40px;
+    padding: 20px 40px;
     margin-left: ${props => props.currentNestingLevel * 60}px;
 `;
