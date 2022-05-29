5028337421d25116092f231093e4d8f03b70ace4
roman_orlov <roman.developer@mail.ru>
2022-05-29 20:50:26 +0500

    Added Modal component

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import { ModalCloseButton, ModalContainer, ModalPage, ModalTitle, ModalTop } from './Modal.styled';
+import closeSVG from '../../../assets/images/icons/close.svg';
 
-const Modal = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Modal = ({ title, children, setActive }) => {
+	const hideModal = () => {
+		setActive(false);
+	};
+
+	return (
+		<ModalPage>
+			<ModalContainer>
+				<ModalTop>
+					<ModalTitle>{title}</ModalTitle>
+					<ModalCloseButton src={closeSVG} onClick={hideModal} />
+				</ModalTop>
+				{children}
+			</ModalContainer>
+		</ModalPage>
+	);
 };
 
 export default Modal;
--- src/components/UI/Modal/Modal.styled.jsx
+++ src/components/UI/Modal/Modal.styled.jsx
@@ -1,1 +1,55 @@
 import styledComponents from 'styled-components';
+
+export const ModalPage = styledComponents.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+
+	z-index: 9;
+
+	content: '';
+
+	background-color: rgba(0, 0, 0, 0.75);
+`;
+
+export const ModalContainer = styledComponents.div`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+
+	max-width: 740px;
+	width: 95%;
+
+	transform: translate(-50%, -50%);
+
+	background-color: #1D2125;
+	border-radius: 15px;
+
+	padding: 50px 60px;
+`;
+
+export const ModalTop = styledComponents.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	margin-bottom: 50px;
+`;
+
+export const ModalTitle = styledComponents.h2`
+	font-size: 22px;
+	font-weight: 600;
+	color: #ffffff;
+`;
+
+export const ModalCloseButton = styledComponents.img`
+	cursor: pointer;
+
+	transition: .5s;
+
+	&:hover {
+		opacity: 0.6;
+	}
+`;
